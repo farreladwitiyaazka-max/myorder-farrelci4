49fbbe8938e27ea46a3258b0389349dc25d3a505
farreladwitiyaazka-max <farreladwitiyaazka@gmail.com>
2026-07-07 09:43:31 +0700

    Update UserSeeder.php

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -8,17 +8,9 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        $this->db->table('users')->insertBatch([
-            [
-                'username' => 'admin',
-                'password' => password_hash('admin123', PASSWORD_DEFAULT),
-                'role' => 'admin'
-            ],
-            [
-                'username' => 'user',
-                'password' => password_hash('user123', PASSWORD_DEFAULT),
-                'role' => 'user'
-            ]
+        $this->db->table('users')->insert([
+            'username' => 'admin',
+            'role'     => 'admin'
         ]);
     }
 }
